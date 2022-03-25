c894a512a915d69e554f905cf90347aa99577a8d
Sunny996 <satlaakash@gmail.com>
2022-03-25 09:56:38 +0530

    ADDED:A method to print Fibonacc series

--- src/main/LogicalPrograms.java
+++ src/main/LogicalPrograms.java
@@ -1,10 +1,6 @@
 package main;
 
 public class LogicalPrograms {
-    public static void main(String[] args) {
-        LogicalPrograms logicalPrograms = new LogicalPrograms();
-        logicalPrograms.fibonacci(4);
-    }
     public void fibonacci(int n){
         int first=0;
         int second=1;
---
+++ src/main/Test.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Test {
+    public static void main(String[] args) {
+        LogicalPrograms logicalPrograms = new LogicalPrograms();
+        logicalPrograms.fibonacci(4);
+    }
+}
