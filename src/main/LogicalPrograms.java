93aa9727822e399908c56941b60d02d5b0ee4af8
Sunny996 <satlaakash@gmail.com>
2022-03-25 16:29:56 +0530

    ADDED: A method to find if a number is perfect number

--- src/main/LogicalPrograms.java
+++ src/main/LogicalPrograms.java
@@ -1,6 +1,19 @@
 package main;
 
 public class LogicalPrograms {
+    public void fibonacci(int n){
+        int first=0;
+        int second=1;
+        int temp=0;
+        System.out.println(first);
+        System.out.println(second);
+        for(int i=2;i<n;i++){
+            temp=first+second;
+            first=second;
+            second=temp;
+            System.out.println(temp);
+        }
+    }
     public void perfectNumber(int n) {
         int sum = 1;
         for (int i = 2; i < n; i++) {
