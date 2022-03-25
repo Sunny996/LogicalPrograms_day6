d1b9d44963da7649bdc5532ea1d8e7beffbdea80
Sunny996 <satlaakash@gmail.com>
2022-03-25 16:41:30 +0530

    ADDED: A method to find if a number is prime or not

--- src/main/LogicalPrograms.java
+++ src/main/LogicalPrograms.java
@@ -23,4 +23,16 @@ public class LogicalPrograms {
         if (n == sum)
             System.out.println(n + " is a perfect number");
     }
+    public void primeNumber(int n){
+        boolean flag=false;
+        for(int i=2;i<n;i++){
+            if(n%i==0)
+                flag=true;
+        }
+        if(!flag)
+            System.out.println(n+" is a prime number");
+        else
+            System.out.println(n+" is not a prime number");
+
+    }
 }
